2eded0156d6756873e1bc1eb2af7278318c5cdc7
Benji-devw <ben.nav@pm.me>
2025-01-29 09:15:33 +0100

    add json for states select

--- src/Home.jsx
+++ src/Home.jsx
@@ -12,6 +12,8 @@ import { Link } from "react-router";
 import { useDispatch, useSelector } from "react-redux";
 import { createEmployee, setIsLoading } from "./redux/createEmployeeSlice";
 import { Modal } from '@benji-devw/dev-docs';
+import statesList from "../public/statesList.json";
+
 
 const Home = () => {
     const dispatch = useDispatch();
@@ -176,49 +178,11 @@ const Home = () => {
                                 onChange={handleChange}
                             >
                                 <MenuItem value=''>Select a state</MenuItem>
-                                <MenuItem value='AL'>Alabama</MenuItem>
-                                <MenuItem value='AK'>Alaska</MenuItem>
-                                <MenuItem value='AZ'>Arizona</MenuItem>
-                                <MenuItem value='AR'>Arkansas</MenuItem>
-                                <MenuItem value='CA'>California</MenuItem>
-                                <MenuItem value='CO'>Colorado</MenuItem>
-                                <MenuItem value='CT'>Connecticut</MenuItem>
-                                <MenuItem value='DE'>Delaware</MenuItem>
-                                <MenuItem value='DC'>
-                                    District Of Columbia
-                                </MenuItem>
-                                <MenuItem value='FL'>Florida</MenuItem>
-                                <MenuItem value='GA'>Georgia</MenuItem>
-                                <MenuItem value='HI'>Hawaii</MenuItem>
-                                <MenuItem value='ID'>Idaho</MenuItem>
-                                <MenuItem value='IL'>Illinois</MenuItem>
-                                <MenuItem value='IN'>Indiana</MenuItem>
-                                <MenuItem value='IA'>Iowa</MenuItem>
-                                <MenuItem value='KS'>Kansas</MenuItem>
-                                <MenuItem value='KY'>Kentucky</MenuItem>
-                                <MenuItem value='LA'>Louisiana</MenuItem>
-                                <MenuItem value='ME'>Maine</MenuItem>
-                                <MenuItem value='MD'>Maryland</MenuItem>
-                                <MenuItem value='MA'>Massachusetts</MenuItem>
-                                <MenuItem value='MI'>Michigan</MenuItem>
-                                <MenuItem value='MN'>Minnesota</MenuItem>
-                                <MenuItem value='MS'>Mississippi</MenuItem>
-                                <MenuItem value='MO'>Missouri</MenuItem>
-                                <MenuItem value='MT'>Montana</MenuItem>
-                                <MenuItem value='NE'>Nebraska</MenuItem>
-                                <MenuItem value='NV'>Nevada</MenuItem>
-                                <MenuItem value='NH'>New Hampshire</MenuItem>
-                                <MenuItem value='NJ'>New Jersey</MenuItem>
-                                <MenuItem value='NM'>New Mexico</MenuItem>
-                                <MenuItem value='NY'>New York</MenuItem>
-                                <MenuItem value='NC'>North Carolina</MenuItem>
-                                <MenuItem value='ND'>North Dakota</MenuItem>
-                                <MenuItem value='OH'>Ohio</MenuItem>
-                                <MenuItem value='OK'>Oklahoma</MenuItem>
-                                <MenuItem value='OR'>Oregon</MenuItem>
-                                <MenuItem value='PA'>Pennsylvania</MenuItem>
-                                <MenuItem value='RI'>Rhode Island</MenuItem>
-                                <MenuItem value='SC'>South Carolina</MenuItem>
+                                {statesList.map((state) => (
+                                    <MenuItem key={state.abbreviation} value={state.value}>
+                                        {state.value}
+                                    </MenuItem>
+                                ))}
                             </Select>
                         </FormControl>
                     </Grid>
